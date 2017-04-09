07dd07ffd6e343853fafdf0be28a148dcc2989ea
JinHo Do <greatdjh@gmail.com>
2017-04-09 21:27:55 +0900

    change ips

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const API_URL = process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:8080' : 'http://52.78.86.76:8080';
+export const API_URL = process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:8080' : 'http://52.78.28.206:8080';
 
 export const FACEBOOK_APP_ID = process.env.NODE_ENV === 'development' ? '910249799115871' : '910249799115871';
